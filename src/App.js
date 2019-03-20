7d803f166807d5fe5e88dafb56fc6f6af409f179
dpayton23 <darien.payton71@gmail.com>
2019-03-20 16:50:58 -0400

    added toDoList array (data)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
+
+const toDoList = [
+  {
+    task: 'Finish React project',
+    // id:
+    completed: false
+  },
+  {
+    task: 'Work on Single Variable Calculus homework',
+    completed: false
+  },
+  {
+    task: 'Attend project meetup',
+    completed: false
+  },
+  {
+    task: 'Read React.js documentation',
+    completed: false
+  }
+]
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
